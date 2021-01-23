9ec82bea12fe53828247ff602df7576b7679bf7a
lido22 <ahmedwa1999@gmail.com>
2021-01-23 19:12:19 +0200

    first commit

---
+++ models.py
@@ -0,0 +1,39 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        self.conv4 = nn.Conv2d(128, 256, 5)
+        self.fc1 = nn.Linear(256*10*10, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 136)
+        self.drop = nn.Dropout(0.3)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+
+        
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = x.view(x.size(0), -1)
+        x = self.drop(x)
+        x = self.drop(F.relu(self.fc1(x)))
+        x = self.drop(F.relu(self.fc2(x)))
+        x = self.fc3(x)
+                  
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
